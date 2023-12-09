f0b8591b50dc760620b0288554d15eea8441fd03
cristinedior <148567509+cristinedior@users.noreply.github.com>
2023-12-09 15:57:43 -0600

    added html structure and js notes

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+// DOM function needed
+// 1. Get API key
+// 2. Get form element
+// 3. Get city input element
+// 4. Get current weather section element
+// 5. Get forecast section element
+// 6. Get history list element
+// 7. Add event listener for form submission
+// 8. Add event listener for history list items
+// 9. Get weather data from API
+
+// local storage call
+// 1. Get local storage
