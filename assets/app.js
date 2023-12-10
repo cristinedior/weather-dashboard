11622fb2fd90c1cee99937a1a200fa8dd98fb5f2
cristinedior <148567509+cristinedior@users.noreply.github.com>
2023-12-09 18:29:49 -0600

    started on js, plus to-do notes

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,24 @@
-// DOM function needed
-// 1. Get API key
-// 2. Get form element
-// 3. Get city input element
-// 4. Get current weather section element
-// 5. Get forecast section element
-// 6. Get history list element
-// 7. Add event listener for form submission
-// 8. Add event listener for history list items
-// 9. Get weather data from API
+document.addEventListener('DOMContentLoaded', function () {
+  const apiKey = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=cb47a85dd08dbea1301282eef46f7c17';
+  const form = document.getElementById('search-form');
+  const cityInput = document.getElementById('city-input');
+  const currentWeatherSection = document.getElementById('current-weather');
+  const forecastSection = document.getElementById('forecast');
+  const historyList = document.getElementById('history-list');
 
-// local storage call
-// 1. Get local storage
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const cityName = cityInput.value.trim();
+     if (cityName !== '') {
+
+      getWeatherData(cityName);
+    }
+  });
+
+ //need the history list
+ //add event listener to history list
+ //add local storage for search history and recall
+ //'get weather data' in specs listed on module homework
+ //^^ city, date, temperature, an icon, humidity, wind
+ //5 day listing of forecast with above specs
+ 
